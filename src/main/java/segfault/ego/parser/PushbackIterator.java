edccf7dc956bb52805a53292602f52b0969d068f
Jarosław Pałka <jaroslaw.palka@symentis.pl>
2020-04-13 23:05:56 +0200

    clean up of pushback iterator

--- src/main/java/segfault/ego/parser/PushbackIterator.java
+++ src/main/java/segfault/ego/parser/PushbackIterator.java
@@ -4,37 +4,26 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Iterator;
 
-public class PushbackIterator<E> implements Iterator<E>
-{
+public class PushbackIterator<E> implements Iterator<E> {
+
     private final Iterator<E> iterator;
     private final Deque<E> items = new ArrayDeque<>();
 
-    private E current;
-
-    public PushbackIterator( Iterator<E> iterator )
-    {
+    public PushbackIterator(Iterator<E> iterator) {
         this.iterator = iterator;
     }
 
     @Override
-    public boolean hasNext()
-    {
+    public boolean hasNext() {
         return !items.isEmpty() ? true : iterator.hasNext();
     }
 
     @Override
-    public E next()
-    {
-        return (current = !items.isEmpty() ? items.pop() : iterator.next());
+    public E next() {
+        return !items.isEmpty() ? items.pop() : iterator.next();
     }
 
-    public E current()
-    {
-        return current;
-    }
-
-    public void pushback( E element )
-    {
-        items.push( element );
+    public void pushback(E element) {
+        items.push(element);
     }
 }
