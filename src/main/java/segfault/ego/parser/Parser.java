bb87262b60e368a9572b14755378a307c1b31b71
Jarosław Pałka <jaroslaw.palka@symentis.pl>
2020-04-15 14:47:44 +0200

    moved tests from main to JUnit

--- src/main/java/segfault/ego/parser/Parser.java
+++ src/main/java/segfault/ego/parser/Parser.java
@@ -28,49 +28,6 @@ import segfault.ego.lexer.Token;
 
 public class Parser {
 
-    public static void main(String[] args) {
-
-        var lexer = new Lexer();
-        var parser = new Parser();
-
-        var expr = parser.parse(lexer.tokenize("()"));
-        assert listExpr().equals(expr);
-
-        expr = parser.parse(lexer.tokenize("(())"));
-        assert listExpr(
-                    listExpr())
-                .equals(expr);
-
-        expr = parser.parse(lexer.tokenize("((atom))"));
-        assert listExpr(
-                    listExpr(
-                        atomExpr("atom")))
-                .equals(expr);
-
-        expr = parser.parse(lexer.tokenize("((atom))"));
-        assert listExpr(
-                    listExpr(
-                        atomExpr("atom")))
-                .equals(expr);
-    
-        expr = parser.parse(lexer.tokenize("((atom)())"));
-        assert listExpr(
-                    listExpr(
-                        atomExpr("atom")),
-                    listExpr())
-                .equals(expr);
-
-        expr = parser.parse(lexer.tokenize("""
-        ((atom (help "Me")))"""));
-        assert listExpr(
-                        listExpr( 
-                            atomExpr("atom"),
-                            listExpr( 
-                                atomExpr("help"),stringLiteralExpr("Me"))))
-                        .equals(expr);
-        
-    }
-
     public Expr parse(List<Token> tokens) {
         var iterator = new PushbackIterator<>(tokens.iterator());
         return exprRule(iterator);
